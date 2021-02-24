3f746b67ae851e8b85f7515b269387cb7dbd4aec
Josh Broughton <josh.broughton47@gmail.com>
2021-02-24 13:35:25 -0700

    created operate function which takes an operator and 2 numbers as the arguments and returns the result of the operator on the two numbers

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+function operate(operator, x, y) {
+    if (operator === "+") {
+        return add(x, y);
+    } else if (operator === "-") {
+        return subtract(x, y);
+    } else if (operator === "X") {
+        return multiply(x, y);
+    } else if (operator === "/") {
+        return divide(x, y);
+    }
+}
