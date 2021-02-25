7f096ecec7bfd8b9c8d6babe24b46f04d073ea85
Josh Broughton <josh.broughton47@gmail.com>
2021-02-25 08:56:44 -0700

    created numberButtons function which saves each click of the numbered buttons into the running variable and updateDisplay function which updates calculator dipslay

--- script.js
+++ script.js
@@ -28,3 +28,23 @@ function operate(operator, x, y) {
 //event listener for = that causes that executes operate with these variables
 //also cause operate to execute if one of the other operators is clicked instead
 //of equals
+let displayNum = "";
+var x;
+var y;
+function numberButtons() {
+    let elements = document.getElementsByClassName("button number");
+    for (i=0;i<elements.length;i++) {
+        thisElement = elements[i];
+        thisElement.addEventListener("click", function() {
+            displayNum += this.textContent;
+            updateDisplay(displayNum);
+        }) 
+    }
+} 
+//adds event listeners to number buttons and concatenates them to string of numbers
+//displayed in display
+function updateDisplay(newText) {
+    let display = document.querySelector(".calc-display");
+    display.innerText = newText;
+}
+numberButtons();    
